bc4ebf24f9c5237cae1002d1f6ed28288ee770bb
callmyaseen <my1476662@gmail.com>
2024-09-22 00:21:17 +0500

    Miner changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ btnEl.addEventListener('click', ()=>{
     if(btnEl.innerHTML === "Dark Mode"){
     containerEl.style.backgroundColor = 'black'
     btnEl.style.backgroundColor = 'black'
+    btnEl.style.border = '2px solid black'
     containerEl.style.color = 'white'
     btnEl.style.color = 'white'
     btnEl.innerHTML = 'Light Mode'
@@ -11,6 +12,7 @@ btnEl.addEventListener('click', ()=>{
     containerEl.style.backgroundColor = 'white'
     btnEl.style.backgroundColor = 'white'
     containerEl.style.color = 'black'
+    btnEl.style.border = '2px solid white'
     btnEl.style.color = 'black'
     btnEl.innerHTML = 'Dark Mode'
     }
